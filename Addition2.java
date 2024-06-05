4f28ed161bbaca38b3739ed2b596312c67f47722
Student AppnaCollage <sabneviraj99@gmail.com>
2024-06-05 11:39:19 -0700

    Add new File

---
+++ Addition2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Addition2
+{
+	public static void main(String [] args)
+	{
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter value");
+		int a = sc.nextInt();
+		System.out.println("Enter Second Value");
+		int b = sc.nextInt();
+		
+		int c ;
+		System.out.println(c = a + b);
+	}
+}
+		
